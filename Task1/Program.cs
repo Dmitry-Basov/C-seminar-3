296b0e38365383f665f85c813371fd13d5160914
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-03 00:52:52 +0300

    seminar3:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая определяет, присутствует ли заданное 
+// число в массиве. Программа должна выдать ответ: Да/Нет
+
+
+// int[] array1 = new int [5];
+// int[] array1 = new int [5] {3, 6, 8 ,3 ,9};
+// int[] array3 = new int [] {3,6,8,3,9};
+int[] array = { 1, 3, 8, 19, 3 };
+int num = 8;
+
+bool isExistNum = false;
+
+for (int i=0; i < array.Length; i++)
+{
+    if(array[i] == num)
+    {
+        isExistNum = true;
+        break;
+    }
+}
+
+
+// string result = isExistNum ? "Да" : "Нет";
+Console.WriteLine(isExistNum ? "Да" : "Нет");
