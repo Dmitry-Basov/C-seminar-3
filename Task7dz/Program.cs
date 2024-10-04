7ae7ab7d5fac57af83174befc551903aec66206a
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-05 02:25:58 +0300

    add seminar3:Task7dz

---
+++ Task7dz/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте массив из 10 целых чисел. Найдите среднее значение элементов
+// массива.
+
+
+
+int[] array = new int[] { 1, 2, 8, 4, 5, 17, 7, 8, 3, 10 };
+
+int i = 0;
+double sum = 0;
+
+while(i < array.Length)
+{
+    sum = sum + array[i];
+    i++;
+}
+double result = sum / array.Length;
+Console.Write(result + " ");
